5709358c57f4ba1e53c92f6bf398cc82a3c6b00a
afterhoon <hooni0517@gmail.com>
2016-11-30 18:54:59 +0900

    Update 04-1-1.cpp

--- Chapter04/04-1-1.cpp
+++ Chapter04/04-1-1.cpp
@@ -1,6 +1,6 @@
 /*
 	문제 1. p142 의 FruitSaleSim1.cpp 를 정보은닉 하시오
-			이하는 FruitSaleSim1.cpp
+		변경된 FruitSaleSim1.cpp
 */
 
 #include <iostream>
@@ -24,7 +24,7 @@ public:
 		myMoney += money;
 		return num;
 	}
-	void ShowSalesResult() {
+	void ShowSalesResult() const {
 		cout << "남은 사과: " << numOfApples << endl;
 		cout << "판매 수익: " << myMoney << endl << endl;
 	}
@@ -40,11 +40,17 @@ public:
 		myMoney = money;
 		numOfApples = 0;
 	}
-	void BuyApples(FruitSeller &seller, int money) {
-		numOfApples += money / seller.SaleApples(money);
+	bool BuyApples(FruitSeller &seller, int money) {
+		if (money < 0) {
+			cout << "사과를 구매하려면 0원 이상을 주어야 합니다." << endl;
+			return false;
+		}
+			
+		numOfApples += seller.SaleApples(money);
 		myMoney -= money;
+		return true;
 	}
-	void ShowBuyResult() {
+	void ShowBuyResult() const {
 		cout << "현재 잔액: " << myMoney << endl;
 		cout << "사과 개수: " << numOfApples << endl;
 	}
@@ -55,8 +61,13 @@ int main() {
 	seller.InitMembers(1000, 20, 0);
 	FruitBuyer buyer;
 	buyer.InitMembers(5000);
-	buyer.BuyApples(seller, 2000);
 
+	if (!buyer.BuyApples(seller, -1000))
+		cout << "구매 실패" << endl;
+
+	if(!buyer.BuyApples(seller, 2000))
+		cout << "구매 실패" << endl;
+	
 	cout << "과일 판매자의 현황" << endl;
 	seller.ShowSalesResult();
 	cout << "과일 구매자의 현황" << endl;
